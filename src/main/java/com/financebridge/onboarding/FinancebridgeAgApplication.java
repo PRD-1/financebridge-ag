9bcbae06e56ce065d5654f3c79f069aba31231c5
PraneethDeshpande <51740479+PraneethDeshpande@users.noreply.github.com>
2026-06-08 19:43:42 +0530

    Initial commit: set up FinanceBridge AG onboarding service

---
+++ src/main/java/com/financebridge/onboarding/FinancebridgeAgApplication.java
@@ -0,0 +1,13 @@
+package com.financebridge.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancebridgeAgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancebridgeAgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/financebridge/onboarding/FinancebridgeAgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financebridge.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancebridgeAgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
